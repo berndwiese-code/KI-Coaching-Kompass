1132eb977a30f79e730e9ca62995103b59195659
Bernd Wiese <bernd.wiese@googlemail.com>
2026-04-06 09:14:56 +0200

    Sanity: CLI auf 5.17.1 upgraden, runtime auf 5.17.1 angleichen

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,14 +5,4 @@ export default defineCliConfig({
     projectId: "w8oq2446",
     dataset: "production",
   },
-  vite: (config) => ({
-    ...config,
-    optimizeDeps: {
-      ...config.optimizeDeps,
-      exclude: [
-        ...((config.optimizeDeps?.exclude) ?? []),
-        "@tanstack/react-table",
-      ],
-    },
-  }),
 });
